24274fa4f844880895d9e99792b518c474471fb8
Arthurburkey <Arthurburkey@email.com>
2017-07-08 17:05:46 -0700

    Printing text in C# prompt
    Drill 9

---
+++ Slide9.cs
@@ -0,0 +1,23 @@
+//Written by Arthur A. Burkey III
+//DRILL(slide-9) 
+//Complete drill in, "Building C# Applications Using csc.exe" on pages 36-38
+//in book, "Pro C# 2008 and the .NET 3.5 Platform".
+//upload on Github when finished//
+
+// simple  C# application.
+using System;
+
+class TestApp
+{
+	static void Main()
+	{
+		Console.WriteLine("Testing! 1, 2, 3");
+	}
+}
+
+
+//Commands Used in VS Cmd Prompt:
+//cd C:\Users\Student\Desktop\ClassCoding\C#
+//csc /target:exe Slide9.cs
+//csc /t:exe Slide9.cs
+//csc Slide9.cs
